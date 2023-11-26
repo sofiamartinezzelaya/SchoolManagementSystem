8a44eba1a4bc28c4930a0da79bec5580ea07c37a
sofiamartinezzelaya <6254086@edu.vaniercollege.qc.ca>
2023-11-26 15:27:48 -0500

    removed the SimpleCalender classes

--- src/main/java/org/sofia/dto/Department.java
+++ src/main/java/org/sofia/dto/Department.java
@@ -1,3 +1,5 @@
+package org.sofia.dto;
+
 public class Department {
     private String department;
 }
---
+++ src/main/java/org/sofia/dto/Main.java
@@ -0,0 +1,12 @@
+package org.sofia.dto;
+
+/**
+ * Simple Calender
+ *
+ * @author Sofia Martinez
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
