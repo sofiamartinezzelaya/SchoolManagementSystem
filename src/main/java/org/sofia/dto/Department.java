8102df74691e4aebd071f4afd2464eb1b30312d2
sofiamartinezzelaya <6254086@edu.vaniercollege.qc.ca>
2023-11-26 21:23:12 -0500

    Department method toString()

--- src/main/java/org/sofia/dto/Department.java
+++ src/main/java/org/sofia/dto/Department.java
@@ -1,11 +1,27 @@
 package org.sofia.dto;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
-@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Department {
     private static int nextId = 1;
     private String id;
     private String departmentName;
+
+    /**
+     * intializes all the fields
+     * @param id String of department
+     * @param nextId int that increases the id of the department when a new department is added
+     * @param departmentName String of the department name
+     * @return the id and department name
+     */
+    public String toString(String id, int nextId, String departmentName) {
+        return null; //filler for empty method, will be replaced during completion
+    }
 
 }
