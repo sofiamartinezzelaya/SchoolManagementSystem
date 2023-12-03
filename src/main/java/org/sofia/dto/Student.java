32feeff9d01957d684c627b740d3dba2be49afd9
sofiamartinezzelaya <6254086@edu.vaniercollege.qc.ca>
2023-12-03 14:58:42 -0500

    Student constructor

--- src/main/java/org/sofia/dto/Student.java
+++ src/main/java/org/sofia/dto/Student.java
@@ -3,12 +3,10 @@ package org.sofia.dto;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @EqualsAndHashCode
 @Getter
 @Setter
-@ToString
 public class Student {
     private String fname;
     private String lname;
@@ -21,8 +19,17 @@ public class Student {
     private static int nextId = 1;
 
     public Student(String fname, String lname, Department department) {
+        this.studentId = String.format("S%03d", nextId++);
+        this.fname = fname;
+        this.lname = lname;
+        this.department = department;
 
     }
+
+    @Override
+     public String toString() {
+        return studentId + fname + lname + department + courseNum + courses;
+     }
 
 
 
