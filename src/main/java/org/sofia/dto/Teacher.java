161be6fc59b0aed003ceb0e3b9aea1c63268b080
sofiamartinezzelaya <6254086@edu.vaniercollege.qc.ca>
2023-12-03 15:18:17 -0500

    Teacher constructor

--- src/main/java/org/sofia/dto/Teacher.java
+++ src/main/java/org/sofia/dto/Teacher.java
@@ -3,20 +3,25 @@ package org.sofia.dto;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @EqualsAndHashCode
 @Getter
 @Setter
-@ToString
 public class Teacher {
     private String fname;
     private String lname;
     private Department department;
-    private String TeacherId;
+    private String teacherId;
     private static int nextId = 1;
 
     public Teacher(String fname, String lname, Department department) {
+        this.teacherId = String.format("T%03d", nextId++);
+        this.fname = fname;
+        this.lname = lname;
+        this.department = department;
+    }
 
+    public String toString() {
+        return teacherId + fname + lname + department;
     }
 }
